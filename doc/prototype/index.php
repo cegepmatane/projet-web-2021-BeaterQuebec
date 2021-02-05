74f79cf5488c57684d1a6500ebbe121574dcec48
Antoine Bélanger <31995891+ThonyDelire@users.noreply.github.com>
2021-02-05 15:35:11 -0500

    Prototype accueil et mission html #5 #4

---
+++ doc/prototype/index.php
@@ -0,0 +1,5 @@
+<?php require "configuration.php"; include "entete.php"; include "getVisiteurInfos.php";?>
+<header><h2>Acceuil</h2></header>
+<br>
+<h3>Bienvenue sur la boutique en ligne d'achat de voitures usagers #1 du Québec!</h3>
+<?php include "pied-page.php"?>
