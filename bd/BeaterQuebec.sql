dfe70acda4ba6e246a7b1f764561d32582499f65
Antoine Bélanger <31995891+ThonyDelire@users.noreply.github.com>
2021-02-09 20:36:00 -0500

    Base de donnée + index + mission #14
    Base de donnée plus index.php et mission.php

---
+++ bd/BeaterQuebec.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 03 fév. 2021 à 06:19
+-- Version du serveur :  10.4.17-MariaDB
+-- Version de PHP : 7.3.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `BeaterQuebec`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `membres`
+--
+
+CREATE TABLE `membres` (
+  `id` int(11) NOT NULL,
+  `pseudonyme` varchar(255) NOT NULL,
+  `motDePasse` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `membres`
+--
+
+INSERT INTO `membres` (`id`, `pseudonyme`, `motDePasse`) VALUES
+(1, 'Charles', '12345'),
+(2, 'Antoine', '1234'),
+(3, 'test', '123'),
+(4, 'test420', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `voitures`
+--
+
+CREATE TABLE `voitures` (
+  `id` int(11) NOT NULL,
+  `marque` varchar(255) NOT NULL,
+  `modele` varchar(255) NOT NULL,
+  `annee` int(11) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `prix` int NOT NULL,
+  `vendu` BOOLEAN DEFAULT false,
+  `image` varchar(255) DEFAULT NULL,
+  `miniature` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `voitures`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `visiteurs`
+--
+
+CREATE TABLE `visiteurs` (
+  `id` int(11) NOT NULL,
+  `user_ip` varchar(255) NOT NULL,
+  `page` varchar(255) NOT NULL,
+  `parametre` varchar(255) NOT NULL,
+  `langue` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `membres`
+--
+ALTER TABLE `membres`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `voitures`
+--
+ALTER TABLE `voitures`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `visiteurs`
+--
+ALTER TABLE `visiteurs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `membres`
+--
+ALTER TABLE `membres`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `voitures`
+--
+ALTER TABLE `voitures`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `visiteurs`
+--
+ALTER TABLE `visiteurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ mission.php
@@ -0,0 +1,5 @@
+<?php require "configuration.php"; include "entete.php"; include "getVisiteurInfos.php";?>
+<header><h2>Notre mission</h2></header>
+<br>
+<h3>Notre mission est de faire ne sorte que chaque beater trouve son propriétaire et le rende le plus heureux possible. De plus, nous voulons offrir le meilleur service de vente dans le domaine des voitures usagées.</h3>
+<?php include "pied-page.php"?>
