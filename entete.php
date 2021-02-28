c4164ae2fa8ef87c4b98b5c02c2e2c8cccce987c
CGMATANE\1734220 <dont0touch@gmail.com>
2021-02-28 16:28:13 -0500

    #19 tranduction de la page profil

--- entete.php
+++ entete.php
@@ -33,18 +33,20 @@ textdomain($domain);
     <?php session_start();?>
         
     <div class="header">
-      <a href="index.php?lang=<?php echo $urlLocale ?>" class="logo"><?php echo _("Les Beaters du Québec"); ?></a>
+      <a href="index.php?lang=<?php echo $urlLocale; ?>" class="logo"><?php echo _("Les Beaters du Québec"); ?></a>
       <div class="header-right">
-        <a <?php $page = basename($_SERVER['PHP_SELF']); if ($page == "index.php"){echo 'class="active"';}?> href="index.php?lang=<?php echo $urlLocale ?>"><?php echo _("Acceuil"); ?></a>
-        <a <?php $page = basename($_SERVER['PHP_SELF']); if ($page == "a-propos.php"){echo 'class="active"';}?> href="a-propos.php?lang=<?php echo $urlLocale ?>"><?php echo _("À propos"); ?></a>
-        <a <?php $page = basename($_SERVER['PHP_SELF']); if ($page == "liste-voiture.php"){echo 'class="active"';}?> href="liste-voiture.php?lang=<?php echo $urlLocale ?>"><?php echo _("Voir les offres"); ?></a>
-        <a <?php $page = basename($_SERVER['PHP_SELF']); if ($page == "membre.php"){echo 'class="active"';}?> href="membre.php?lang=<?php echo $urlLocale ?>"><?php echo _("Membre"); ?></a>
+        <a <?php $page = basename($_SERVER['PHP_SELF']); if ($page == "index.php"){echo 'class="active"';}?> href="index.php?lang=<?php echo $urlLocale; ?>"><?php echo _("Acceuil"); ?></a>
+        <a <?php $page = basename($_SERVER['PHP_SELF']); if ($page == "a-propos.php"){echo 'class="active"';}?> href="a-propos.php?lang=<?php echo $urlLocale; ?>"><?php echo _("À propos"); ?></a>
+        <a <?php $page = basename($_SERVER['PHP_SELF']); if ($page == "liste-voiture.php"){echo 'class="active"';}?> href="liste-voiture.php?lang=<?php echo $urlLocale; ?>"><?php echo _("Voir les offres"); ?></a>
+        <a <?php $page = basename($_SERVER['PHP_SELF']); if ($page == "membre.php"){echo 'class="active"';}?> href="membre.php?lang=<?php echo $urlLocale; ?>"><?php echo _("Membre"); ?></a>
         <?php 
           $page = basename($_SERVER["PHP_SELF"]); 
           
           if (!empty($_SESSION)){ 
               if ($_SESSION["admin"]){
-                  echo '<a href="administration/index.php">';
+                  echo '<a href="administration/index.php?lang=';
+                  echo $urlLocale;
+                  echo'">';
                   echo _('Panneau Admin');
                   echo'</a>';
               }
--- profil.php
+++ profil.php
@@ -1,43 +1,43 @@
 
-<a style="float:right;" class="boutonThemeMenu" href="traitement-deconnection.php">Se déconnecter</a>
-<header><h2>Profil</h2></header>
+<a style="float:right;" class="boutonThemeMenu" href="traitement-deconnection.php"><?php echo _("Se déconnecter"); ?></a>
+<header><h2><?php echo _("Profile"); ?></h2></header>
 <section id="espace-membre">
-    <p>Bienvenue, <?php echo $_SESSION['pseudonyme']; ?>!</p>
+    <p><?php echo _("Bienvenue"); ?>, <?php echo $_SESSION['pseudonyme']; ?>!</p>
     <form action="traitement-membre-info.php" method="post" enctype="multipart/form-data">
         
         <div class="champs">
-            <label for="pseudonyme">Pseudonyme *</label>
+            <label for="pseudonyme"><?php echo _("Pseudonyme"); ?> *</label>
             <input type="text" name="pseudonyme" id="pseudonyme" value=<?php echo "\"".$_SESSION['pseudonyme']."\""; ?> required/>			
         </div>
         <div class="champs">
-            <label for="email">Addesse courriel *</label>
+            <label for="email"><?php echo _("Addesse courriel"); ?> *</label>
             <input type="email" name="email" id="email" value=<?php echo "\"".$_SESSION['email']."\""; ?> required/>			
         </div>
         <div class="champs">
-            <label for="nom">Nom</label>
+            <label for="nom"><?php echo _("Nom"); ?></label>
             <input type="text" name="nom" id="nom" value=<?php echo "\"".$_SESSION['nom']."\""; ?>/>			
         </div>
         
         <div class="champs">
-            <label for="prenom">Prenom</label>
+            <label for="prenom"><?php echo _("Prenom"); ?></label>
             <input type="text" name="prenom" id="prenom" value=<?php echo "\"".$_SESSION['prenom']."\""; ?>/>			
         </div>
-        <input type="submit" value="Modifier vos informations">
+        <input type="submit" value="<?php echo _("Modifier vos informations"); ?>">
     </form>
 
     <form action="traitement-membre-mdp.php" method="post" enctype="multipart/form-data">
         <div class="champs">
-            <label for="ancienMotDePasse">Ancien mot de passe *</label>
+            <label for="ancienMotDePasse"><?php echo _("Ancien mot de passe"); ?> *</label>
             <input type="password" name="ancienMotDePasse" id="ancienMotDePasse" required/>			
         </div>
         
         <div class="champs">
-            <label for="confirmationMotDePasse">Nouveau mot de passe *</label>
+            <label for="confirmationMotDePasse"><?php echo _("Nouveau mot de passe"); ?> *</label>
             <input type="password" name="confirmationMotDePasse" id="confirmationMotDePasse" required/>			
         </div>
-        <input type="submit" value="Modifier votre mot de passe">
+        <input type="submit" value="<?php echo _("Modifier votre mot de passe"); ?>">
     </form>
-    <h1>Historique d'achat</h1>
+    <h1><?php echo _("Historique d'achat"); ?></h1>
     <div style="margin-bottom: 5rem;">
     <?php
         require CHEMIN_ACCESSEUR . "VoitureDAO.php";
