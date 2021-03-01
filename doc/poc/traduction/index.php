965792c69c7e4b7fdaab18c9c8c7addefa41b313
CGMATANE\1734220 <dont0touch@gmail.com>
2021-03-01 00:57:21 -0500

    #20 Ajout de la preuve de concept et des captures d'écran

---
+++ doc/poc/traduction/index.php
@@ -0,0 +1,5 @@
+<?php require "configuration.php"; include "entete.php";?>
+<header><h2><?php echo _("Acceuil"); ?></h2></header>
+<br>
+<h3><?php echo _("Bienvenue sur la boutique en ligne d'achat de voitures usagers #1 du Québec!"); ?></h3>
+<?php include "pied-page.php"?>
